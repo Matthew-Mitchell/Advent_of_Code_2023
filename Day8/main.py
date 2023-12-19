a2c9a58e417da6c2b30b56a1e3f7825a13c0beee
Matthew-Mitchell <mmitchell17@protonmail.com>
2023-12-18 22:17:26 -0500

    Day 8 part2 outline but recursion limit hit.

--- Day8/main.py
+++ Day8/main.py
@@ -1,10 +1,15 @@
 import re
+import sys
 
 class main:
 	def __init__(self):
 		self.neighbors = {}
 		self.stepcount = 0
+		self.stepcount2 = 0
 		self.node = "AAA"
+		self.p2nodes = None
+		#Increase Recursion Limit
+		sys.setrecursionlimit(16385)
 	def read(self, filename):
 		neighbors = self.neighbors
 		with open(filename) as f:
@@ -50,4 +55,30 @@ class main:
 				#print(f"On step: {self.stepcount} from: {cur_node} to: {next_node} via: {c}")				
 		#If ZZZ not found, continue searching!
 		self.pathlength()
+	def pathlength2(self):
+		#follow instructions
+		if self.p2nodes == None:
+			#Initialize p2nodes
+			self.p2nodes = [node for node in self.nodes if node[-1]=="A"]
+		for c in self.directions:
+			self.stepcount += 1
+			LRs = [self.neighbors[node] for node in self.p2nodes]
+			if c == "L":
+				next_nodes = [LR[0] for LR in LRs]
+			elif c == "R":
+				next_nodes = [LR[1] for LR in LRs]
+			else:
+				assert False , f"Hit Error! Encountered: {c} in instructions."
+			self.p2nodes = next_nodes
+			#if zzz terminate
+			if all([n[-1]=="Z" for n in self.p2nodes]):
+				print(f"On step: {self.stepcount} from: {self.p2nodes} to: {next_nodes} via: {c}")
+				print(f"Journey Completed! Process took: {self.stepcount}")
+				#Exit
+				return None
+			elif self.stepcount % 100 == 0:
+				pass
+				sys.stdout.write(f"\rOn step: {self.stepcount} from: {self.p2nodes[:2]} to: {next_nodes[:2]} via: {c}")
+		#If ZZZ not found, continue searching!
+		self.pathlength2()
 
